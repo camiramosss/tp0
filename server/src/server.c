9b2e753c20d3a5ea62de0c3baec8837f50e92705
camiramosss <caramos@frba.utn.edu.arm>
2025-04-16 16:27:44 +0000

    tp0 terminado

--- server/src/server.c
+++ server/src/server.c
@@ -3,9 +3,9 @@
 int main(void) {
 	logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
 
-	int server_fd = iniciar_servidor();
+	int server_fd = iniciar_servidor(); //crea un fd para diferenciar la conexion
 	log_info(logger, "Servidor listo para recibir al cliente");
-	int cliente_fd = esperar_cliente(server_fd);
+	int cliente_fd = esperar_cliente(server_fd); //se queda esperando al cliente
 
 	t_list* lista;
 	while (1) {
